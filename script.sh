14c098a706dc15e29f5585b1ddac5e1cf28d3a96
i-dev <ishadowdev@gmail.com>
2025-11-19 14:54:22 +0330

    Update

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 cd
 sudo mkdir 1
 cd 1
-sudo wget https://raw.githubusercontent.com/ParsaKSH/Warp-Multi-IP/refs/heads/main/warp.py
+sudo wget https://raw.githubusercontent.com/iliya-Developer/Warp-Multi-IP/refs/heads/main/warp.py
 chmod +x warp.py
 sudo python3 warp.py
